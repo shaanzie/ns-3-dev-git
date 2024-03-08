ae9750b36ca0227917b5310be74e68d46b798e8d
shaanzie <ishaanlagwankar@gmail.com>
2024-03-07 22:17:17 -0500

    Adding rep sim

---
+++ scratch/build.sh
@@ -0,0 +1,67 @@
+NUM_PROCS=32
+
+# ALPHA - Message rate (milliseconds)
+ALPHA=500
+
+# DELTA - Message delay (milliseconds)
+DELTA=200
+
+INTERVAL=10
+
+EPSILON=30
+
+# Function to calculate the number of bits required to store an integer
+num_bits() {
+    number=$1
+    bits=0
+    while ((number > 0)); do
+        ((bits++))
+        number=$((number >> 1))
+    done
+    echo $bits
+}
+
+
+
+today=$(date +"%Y-%m-%d")
+
+
+for (( NUM_PROCS=32; NUM_PROCS<=64; NUM_PROCS+=32)); do
+    for (( EPSILON=100; EPSILON<=1000; EPSILON+=50)); do
+        for (( INTERVAL=1; INTERVAL<=$EPSILON; INTERVAL++)); do
+            if (( INTERVAL * EPSILON % 1000 == 0 && INTERVAL*EPSILON <= 6000 )); then
+                for (( DELTA=1; DELTA<=EPSILON; DELTA*=2)); do
+                    for (( ALPHA=10; ALPHA<=320; ALPHA*=2)); do
+                        MAX_OFFSET_SIZE=$(num_bits $EPSILON)
+
+                        echo "N.${NUM_PROCS}-E.${EPSILON}-I.${INTERVAL}-D.${DELTA}-A.${ALPHA}-M.${MAX_OFFSET_SIZE}"    
+                        
+                        touch replay-config.h
+
+                        echo "#define REPCL_CONFIG_H" >> replay-config.h
+
+                        echo "#define NUM_PROCS $NUM_PROCS" >> replay-config.h
+
+                        echo "#define EPSILON $EPSILON" >> replay-config.h
+                        echo "#define INTERVAL $INTERVAL" >> replay-config.h
+                        echo "#define ALPHA $ALPHA" >> replay-config.h
+                        echo "#define DELTA $DELTA" >> replay-config.h
+
+                        echo "#define MAX_OFFSET_SIZE $MAX_OFFSET_SIZE" >> replay-config.h
+
+                        rm src/config-store/model/replay-config.h
+                        mv replay-config.h src/config-store/model/
+
+                        ./ns3 run scratch/replay-simulator.cc > results-${today}-N.${NUM_PROCS}-E.${EPSILON}-I.${INTERVAL}-D.${DELTA}-A.${ALPHA}-M.${MAX_OFFSET_SIZE}.csv 2>&1
+
+                    done
+                done
+            fi
+        done
+    done
+done
+
+
+
+
+# touch scratch/$filename
---
+++ scratch/replay-simulator.cc
@@ -0,0 +1,171 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+// Network topology
+//
+//       n0            n1          n2
+//         \           |           /
+//          \          |          /
+//           \         |         /
+//            \        |        /
+//             \       |       /
+//              \      |      /
+//               \     |     /
+//                \    |    /
+//       n4 ---------  n9 --------- n8
+// - Common echo sink at n9
+
+#include "ns3/applications-module.h"
+#include "ns3/core-module.h"
+#include "ns3/csma-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/config-store.h"
+
+#include <fstream>
+#include <unistd.h>
+
+using namespace ns3;
+
+NS_LOG_COMPONENT_DEFINE("ReplaySimulator");
+
+int
+main(int argc, char* argv[])
+{
+    //
+    // Users may find it convenient to turn on explicit debugging
+    // for selected modules; the below lines suggest how to do this
+    //
+    LogComponentEnable ("ReplaySimulator", LOG_LEVEL_INFO);
+    LogComponentEnable ("ReplayClientServer", LOG_LEVEL_DEBUG);
+
+    NS_LOG_INFO
+    (
+        "Configuration Params: " << 
+        "N." << NUM_PROCS << 
+        ".E." << EPSILON << 
+        ".I." << INTERVAL << 
+        ".D." << DELTA << 
+        ".A." << ALPHA <<
+        ".MAXOFF." << MAX_OFFSET_SIZE
+    );
+
+    //
+    // Explicitly create the nodes required by the topology (shown above).
+    //
+    NS_LOG_INFO("Create nodes.");
+    NodeContainer n;
+
+    n.Create(NUM_PROCS);
+    NS_LOG_INFO("Created " << NUM_PROCS << " nodes.");
+
+    InternetStackHelper internet;
+    internet.Install(n);
+
+    NS_LOG_INFO("Create channels.");
+    //
+    // Explicitly create the channels required by the topology (shown above).
+    //
+    CsmaHelper csma;
+    csma.SetChannelAttribute("DataRate", DataRateValue(DataRate(5000000)));
+
+    csma.SetChannelAttribute("Delay", TimeValue(MilliSeconds(DELTA)));
+
+    NetDeviceContainer d = csma.Install(n);
+
+    //
+    // We've got the "hardware" in place.  Now we need to add IP addresses.
+    //
+    NS_LOG_INFO("Assign IP Addresses.");
+    
+    Ipv4AddressHelper ipv4;
+    ipv4.SetBase("10.1.1.0", "255.255.255.0");
+    Ipv4InterfaceContainer i = ipv4.Assign(d);
+
+    std::vector<Address> peerAddresses;
+
+    for(int p = 0; p < NUM_PROCS; ++ p)
+    {
+        peerAddresses.push_back(Address(i.GetAddress(p)));
+    }
+
+    NS_LOG_INFO("Create Applications.");
+    //
+    // Create a ReplayClientServer application on node 0.
+    //
+    uint16_t port = 9; // well-known echo port number
+
+    ApplicationContainer apps;
+
+    Time interPacketInterval = MilliSeconds(ALPHA);
+
+    for(int i = 1; i < NUM_PROCS; i++)
+    {
+        Ptr<ReplayClientServer> client = new ReplayClientServer();
+        client->SetPort(port);
+        client->SetPeerList(peerAddresses);
+        client->SetSize(1024);
+        client->SetInterval(interPacketInterval);
+
+        Ptr<Node> node = n.Get(i);
+
+        node->AddApplication(client);
+
+        apps.Add(client);
+
+    }
+
+    apps.Start(Seconds(1.0));
+    apps.Stop(Seconds(20.0));
+
+    AsciiTraceHelper ascii;
+    
+    std::stringstream filename;
+
+    filename    << "replay-sim-"
+                << "N." << NUM_PROCS
+                << "E." << EPSILON
+                << "I." << INTERVAL
+                << "D." << DELTA
+                << "A." << ALPHA
+                << "MAXOFFSETSIZE." << MAX_OFFSET_SIZE
+                << ".tr"; 
+
+
+    csma.EnableAsciiAll(ascii.CreateFileStream(filename.str()));
+
+    std::stringstream pcapfile;
+
+    pcapfile    << "PCAP-"
+                << "N." << NUM_PROCS
+                << "E." << EPSILON
+                << "I." << INTERVAL
+                << "D." << DELTA
+                << "A." << ALPHA
+                << "MAXOFFSETSIZE." << MAX_OFFSET_SIZE
+                << ".replay-sim";
+
+    // csma.EnablePcapAll(pcapfile.str(), true);
+
+    //
+    // Now, do the actual simulation.
+    //
+    NS_LOG_INFO("Run Simulation.");
+    NS_LOG_INFO("HLC,BITMAP,OFFSETS,COUNTERS,NUM_PROCS,EPSILON,INTERVAL,DELTA,ALPHA,MAX_OFFSET_SIZE,OFFSET_SIZE,COUNTER_SIZE,CLOCK_SIZE,MAX_OFFSET");
+    Simulator::Run();
+    Simulator::Destroy();
+    NS_LOG_INFO("Done.");
+
+    return 0;
+}
